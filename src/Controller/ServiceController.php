ad067de1f4d9e26ebcf0aa023353a09110b8b6b4
samehsellami <samehsellami@outlook.com>
2025-09-29 20:56:30 +0200

    Premier commit du projet

---
+++ src/Controller/ServiceController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ServiceController extends AbstractController
+{
+    #[Route('/service/{name}', name: 'app_show_service')]
+    public function showService(string $name): Response
+    {
+        return new Response("Service demandé : " . htmlspecialchars($name));
+    }
+
+    #[Route('/service', name: 'app_service')]
+    public function index(): Response
+    {
+        return $this->render('service/index.html.twig', [
+            'controller_name' => 'ServiceController',
+        ]);
+    }
+}
